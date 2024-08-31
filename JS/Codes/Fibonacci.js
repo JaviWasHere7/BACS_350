1c6ab6deab881ae7ca53322d92e88a527c162838
JavierAArroyo-Solis <javierarroyosolis46@gmail.com>
2024-08-30 23:38:05 -0600

    Update Fibonacci.js

--- JS/Codes/Fibonacci.js
+++ JS/Codes/Fibonacci.js
@@ -5,5 +5,12 @@ let fibonacciSequence = (n) => {
         return 1;
     return fibonacciSequence(n - 1) + fibonacciSequence(n - 2);
 }
+console.log("Fibonacci(0): " + fibonacciSequence(0));
+console.log("Fibonacci(1): " + fibonacciSequence(1));
+console.log("Fibonacci(2): " + fibonacciSequence(2));
+console.log("Fibonacci(3): " + fibonacciSequence(3));
+console.log("Fibonacci(4): " + fibonacciSequence(4));
 console.log("Fibonacci(5): " + fibonacciSequence(5));
+console.log("Fibonacci(6): " + fibonacciSequence(6));
+console.log("Fibonacci(7): " + fibonacciSequence(7));
 console.log("Fibonacci(8): " + fibonacciSequence(8));

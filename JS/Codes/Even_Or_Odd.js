2516f0665c7cf6e3cc7f0fefbb91efedb5e0be1d
JavierAArroyo-Solis <javierarroyosolis46@gmail.com>
2024-08-31 15:20:26 -0600

    Update Even_Or_Odd.js

---
+++ JS/Codes/Even_Or_Odd.js
@@ -0,0 +1,13 @@
+function isEven(n) {
+    if (n < 0) {
+        n = Math.abs(n);
+    }
+    return (n === 0) ? true
+    : (n === 1) ? false
+    : isEven(n - 2);
+}
+
+console.log(isEven(50)); // → true 
+console.log(isEven(75)); // → false 
+console.log(isEven(-1)); // → false 
+console.log(isEven(-2)); // → true
